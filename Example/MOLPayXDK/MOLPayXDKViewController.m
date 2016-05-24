a07953a0e5b1e4df774e5d96aa8ac7c4e465613e
Clement <clement@MOLPays-MacBook-Pro.local>
2016-05-24 14:05:16 +0800

    Release 1.3.0

--- Example/MOLPayXDK/MOLPayXDKViewController.m
+++ Example/MOLPayXDK/MOLPayXDKViewController.m
@@ -39,9 +39,10 @@
                                           @"mp_channel_editing": [NSNumber numberWithBool:NO], // Optional
                                           @"mp_editing_enabled": [NSNumber numberWithBool:NO], // Optional
                                           @"mp_transaction_id": @"", // Optional for transactionRequest
-                                          @"mp_request_type": @"" // Optional for transactionRequest, use anything other than "Receipt"
-                                          //@"mp_filter": @"0", // Optional for debit card transactions only
-//                                          @"mp_custom_css_url": [[NSBundle mainBundle] pathForResource:@"custom.css" ofType:nil] // Optional for custom UI
+                                          @"mp_request_type": @"" // Optional for transactionRequest. Use anything other than "Receipt".
+                                          //@"mp_is_escrow": @"" // Optional for escrow
+                                          //@"mp_filter": @"", // Optional for debit card transactions only
+                                          //@"mp_custom_css_url": [[NSBundle mainBundle] pathForResource:@"custom.css" ofType:nil] // Optional for custom UI
                                           };
     
     mp = [[MOLPayLib alloc] initWithDelegate:self andPaymentDetails:paymentRequestDict];
